203de8e697d46e569760278685d7a63fe2cb8e5d
Alvin <alvinvaati@gmail.com>
2024-03-18 23:45:43 +0300

    Refactor import statements in authentication module

--- api/v1/authentication/forms.py
+++ api/v1/authentication/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import  DataRequired, Email, EqualTo, Length, ValidationError
-from authentication.models import User
+from models import User
 
 """
 Authentication Forms
